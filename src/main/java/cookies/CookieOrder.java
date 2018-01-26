cf6e41c2902ddc19c4b5227ad02c572eff1be27b
AlanKostrick <alan@wecancodeit.org>
2018-01-25 22:06:06 -0500

    first commit

---
+++ src/main/java/cookies/CookieOrder.java
@@ -0,0 +1,27 @@
+package cookies;
+
+public class CookieOrder {
+
+	private String _____;
+	private int ______;
+
+	public CookieOrder() {
+
+	}
+
+	public CookieOrder(String _____, int _____) {
+		this._____ = _______;
+		this._______ = ________;
+	}
+
+	public String ______() {
+
+		return ______;
+	}
+
+	public int ______() {
+
+		return _____;
+	}
+
+}
---
+++ src/test/java/cookies/CookieOrderTest.java
@@ -0,0 +1,42 @@
+package cookies;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CookieOrderTest {
+
+	@Test
+	public void shouldInstantiateACookieOrderObject() {
+		CookieOrder underTest = new CookieOrder();
+
+		assertNotNull(underTest);
+	}
+
+	@Test
+	public void shouldInstantiateACookieObjectWithVarietyAndBoxes() {
+		CookieOrder underTest = new CookieOrder(___, ___);
+
+		__________(underTest);
+	}
+	
+	@Test
+	public void shouldReturnCookieVarietyAsTagalongs() {
+		
+		
+		
+		assertEquals(____, check);
+		
+	}
+	
+	@Test
+	public void shouldReturnNumberOfBoxesAsOne() {
+		
+		
+		
+		assertEquals(____, check);
+	}
+
+
+
+}
