72ad2df066e9e0844be6e3290cdfbbd24d7f7aec
AlanKostrick <alan@wecancodeit.org>
2018-01-26 23:29:58 -0500

    set up tests

--- src/test/java/cookies/CookieOrderTest.java
+++ src/test/java/cookies/CookieOrderTest.java
@@ -6,36 +6,36 @@ import org.junit.Test;
 
 public class CookieOrderTest {
 
-	@Test
-	public void shouldInstantiateACookieOrderObject() {
-		CookieOrder underTest = new CookieOrder();
-
-		assertNotNull(underTest);
-	}
-
-	@Test
-	public void shouldInstantiateACookieObjectWithVarietyAndBoxes() {
-		CookieOrder underTest = new CookieOrder(___, ___);
-
-		__________(underTest);
-	}
-	
-	@Test
-	public void shouldReturnCookieVarietyAsTagalongs() {
-		
-		
-		
-		assertEquals(____, check);
-		
-	}
-	
-	@Test
-	public void shouldReturnNumberOfBoxesAsOne() {
-		
-		
-		
-		assertEquals(____, check);
-	}
+//	@Test
+//	public void shouldInstantiateACookieOrderObject() {
+//		CookieOrder underTest = new CookieOrder();
+//
+//		assertNotNull(underTest);
+//	}
+//
+//	@Test
+//	public void shouldInstantiateACookieObjectWithVarietyAndBoxes() {
+//		CookieOrder underTest = new CookieOrder(___, ___);
+//
+//		__________(underTest);
+//	}
+//	
+//	@Test
+//	public void shouldReturnCookieVarietyAsTagalongs() {
+//		
+//		
+//		
+//		assertEquals(____, check);
+//		
+//	}
+//	
+//	@Test
+//	public void shouldReturnNumberOfBoxesAsOne() {
+//		
+//		
+//		
+//		assertEquals(____, check);
+//	}
 
 
 
